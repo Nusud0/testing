f03c636110ebe9295b542bcb99cc98ba1aff4ede
Apxitector <kosna11kosma11@mail.ru>
2026-03-09 23:32:31 +0300

    add sqlalchemy, alembic miration

---
+++ backend/database.py
@@ -0,0 +1,21 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = "sqlite+aiosqlite:///./text.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+async_session_maker = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
