dfe9baf3735262d625eb1a96277b581753f12a2b
Apxitector <kosna11kosma11@mail.ru>
2026-03-10 22:10:45 +0300

    small fix

--- backend/app/schemas/user_schemas.py
+++ backend/app/schemas/user_schemas.py
@@ -5,10 +5,13 @@ class UserResponse(BaseModel):
     name: str = Field(..., description="User name")
 
     class Config:
-        form_attributes = True
+        from_attributes = True
 
 class UserCreate(BaseModel):
     name: str = Field(..., min_length=3, max_length=50, description="User name")
+
+    class Config:
+        from_attributes = True
 
 class UserListResponse(BaseModel):
     users: list[UserResponse]
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -15,7 +15,7 @@ class UserService:
         return UserListResponse(users=users_response, total=len(users_response))
     
     async def get_user_by_id(self, user_id: int) -> UserResponse:
-        user = await self.user_repository.get_by_id(id)
+        user = await self.user_repository.get_by_id(user_id)
 
         if not user:
             raise HTTPException(
@@ -31,4 +31,4 @@ class UserService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"User data {user_data} not validate"
             )
-        return user
+        return UserResponse.model_validate(user)
